c4575aa4763e1716dfd82cbd45d8875e84a354df
dmitry <kor.dima97@mail.ru>
2024-07-23 14:56:43 +0500

    fix

--- src/Helpers/ConfigHelper.php
+++ src/Helpers/ConfigHelper.php
@@ -4,7 +4,7 @@ namespace Fillincode\Tests\Helpers;
 
 class ConfigHelper
 {
-    public static function get(string $group, string $prefix, string $key): mixed
+    public static function get(string $group, ?string $prefix, string $key): mixed
     {
         return config("fillincode-tests.$group.$prefix.$key") ?: config("fillincode-tests.$group.default.$key");
     }
