de66cdc4283ba19d3c2a58713e44848620bbd75f
dmitry <kor.dima97@mail.ru>
2024-07-23 15:02:11 +0500

    fix

--- src/Helpers/ConfigHelper.php
+++ src/Helpers/ConfigHelper.php
@@ -6,6 +6,10 @@ class ConfigHelper
 {
     public static function get(string $group, ?string $prefix, string $key): mixed
     {
+        if ($group === 'admin_panel') {
+            return config("fillincode-tests.admin_panel.$key");
+        }
+
         return config("fillincode-tests.$group.$prefix.$key") ?: config("fillincode-tests.$group.default.$key");
     }
 }
